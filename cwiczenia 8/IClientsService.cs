3ac082d2876b8a21b141d166eed3d68d5342609c
Igor Bukowski <111287728+Bukvsky@users.noreply.github.com>
2025-05-06 20:00:09 +0200

    Update IClientsService.cs

--- cwiczenia 8/IClientsService.cs
+++ cwiczenia 8/IClientsService.cs
@@ -4,41 +4,17 @@ namespace Tutorial8.Services
 {
     public interface IClientsService
     {
-        /// <summary>
-        /// Pobiera wszystkie wycieczki powiązane z danym klientem
-        /// </summary>
-        /// <param name="clientId">ID klienta</param>
-        /// <returns>Obiekt z danymi klienta i jego wycieczkami lub null jeśli klient nie istnieje</returns>
+        
         Task<ClientTripDTO?> GetClientTrips(int clientId);
 
-        /// <summary>
-        /// Tworzy nowego klienta w bazie danych
-        /// </summary>
-        /// <param name="client">Dane nowego klienta</param>
-        /// <returns>ID utworzonego klienta</returns>
         Task<int> CreateClient(CreateClientDTO client);
 
-        /// <summary>
-        /// Rejestruje klienta na wycieczkę
-        /// </summary>
-        /// <param name="clientId">ID klienta</param>
-        /// <param name="tripId">ID wycieczki</param>
-        /// <returns>True jeśli rejestracja się powiodła, false jeśli nie</returns>
+
         Task<(bool Success, string? ErrorMessage)> RegisterClientForTrip(int clientId, int tripId);
 
-        /// <summary>
-        /// Usuwa rejestrację klienta z wycieczki
-        /// </summary>
-        /// <param name="clientId">ID klienta</param>
-        /// <param name="tripId">ID wycieczki</param>
-        /// <returns>True jeśli usunięcie się powiodło, false jeśli nie</returns>
         Task<(bool Success, string? ErrorMessage)> UnregisterClientFromTrip(int clientId, int tripId);
 
-        /// <summary>
-        /// Sprawdza czy klient istnieje w bazie danych
-        /// </summary>
-        /// <param name="clientId">ID klienta</param>
-        /// <returns>True jeśli klient istnieje, false jeśli nie</returns>
+
         Task<bool> ClientExists(int clientId);
     }
 }
